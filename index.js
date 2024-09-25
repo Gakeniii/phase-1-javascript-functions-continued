396780b4f5c3884d55e5e5d6cca39dc42583713d
Sifa Gakeni Muriithi <gakenisifa@gmail.com>
2024-09-26 01:57:11 +0300

    Continued functions

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate'){
+    return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun();
+
+const mondayWork = function(activity = 'go to the office'){
+    return `This Monday, I will ${activity}.`;
+}
+mondayWork();
+
+function wrapAdjective(star = '*'){
+    return function(content = 'special'){
+        return `You are ${star}${content}${star}!`;
+    };
+}
+wrapAdjective ()('a hard worker');
+wrapAdjective ('||')('a dedicated programmer');
